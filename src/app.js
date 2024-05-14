1e42d2858879fa183853325f0940d302daf493c1
Gabriel Cumare Montilla <gacm95218@gmail.com>
2024-05-14 03:06:38 +0000

    Updated commit to move files

--- src/app.js
+++ src/app.js
@@ -7,5 +7,22 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let bigNumberToDisplay = document.getElementById("numbers");
+
+  const cardSymbols = ["♦", "♥", "♠", "♣"];
+
+  bigNumberToDisplay.innerHTML = Math.floor(Math.random() * 7) + 2;
+
+  let upperIcon = document.getElementById("upperIcon");
+  let lowerIcon = document.getElementById("lowerIcon");
+
+  let randomizerForNumbers = Math.floor(Math.random() * 4);
+
+  if (cardSymbols[randomizerForNumbers] == "♥") {
+    upperIcon.style.color = "red";
+    lowerIcon.style.color = "red";
+  }
+
+  upperIcon.innerHTML = cardSymbols[randomizerForNumbers];
+  lowerIcon.innerHTML = cardSymbols[randomizerForNumbers];
 };
