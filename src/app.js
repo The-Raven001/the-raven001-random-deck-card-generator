3bc6fdb2f0364de889b41392fb6b43ce3d5fff89
Gabriel Cumare Montilla <gacm95218@gmail.com>
2024-05-14 04:24:30 +0000

    Updated Card Generator

--- src/app.js
+++ src/app.js
@@ -5,24 +5,64 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
+function cardGenerator() {
   let bigNumberToDisplay = document.getElementById("numbers");
 
   const cardSymbols = ["♦", "♥", "♠", "♣"];
 
-  bigNumberToDisplay.innerHTML = Math.floor(Math.random() * 7) + 2;
+  const NumberAndLetterToDisplay = [
+    2,
+    3,
+    5,
+    6,
+    7,
+    8,
+    9,
+    10,
+    "Q",
+    "J",
+    "A",
+    "K"
+  ];
+
+  let randomizerForBigNumber = Math.floor(
+    Math.random() * NumberAndLetterToDisplay.length
+  );
+
+  bigNumberToDisplay.innerHTML =
+    NumberAndLetterToDisplay[randomizerForBigNumber];
 
   let upperIcon = document.getElementById("upperIcon");
   let lowerIcon = document.getElementById("lowerIcon");
 
-  let randomizerForNumbers = Math.floor(Math.random() * 4);
+  let randomizerForNumbers = Math.floor(Math.random() * cardSymbols.length);
 
-  if (cardSymbols[randomizerForNumbers] == "♥") {
+  if (
+    cardSymbols[randomizerForNumbers] == "♥" ||
+    cardSymbols[randomizerForNumbers] == "♦"
+  ) {
     upperIcon.style.color = "red";
     lowerIcon.style.color = "red";
+  } else {
+    upperIcon.style.color = "black";
+    lowerIcon.style.color = "black";
   }
 
   upperIcon.innerHTML = cardSymbols[randomizerForNumbers];
   lowerIcon.innerHTML = cardSymbols[randomizerForNumbers];
+}
+
+window.onload = function() {
+  //write your code here
+  cardGenerator();
+
+  setInterval(() => {
+    cardGenerator();
+  }, 10000);
+
+  let buttonCardChanger = document.getElementById("buttonCardChanger");
+
+  buttonCardChanger.addEventListener("click", function() {
+    cardGenerator();
+  });
 };
